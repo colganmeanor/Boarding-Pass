33b137fbd86f6e8feca7d01760b0a48ab0df3fbf
Colgan Meanor <colganmeanor7@gmail.com>
2021-11-11 21:35:09 -0600

    Create Trip class and Traveler Class along with basic test files

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,2 +1,27 @@
-import Traveler from '..src/traveler'
-import travelers from '/test-data.js'
+const chai = require('chai');
+const expect = chai.expect;
+
+import Trip from '../src/trips'
+import Traveler from '../src/traveler'
+import {travelers, trips} from './/test-data.js'
+
+
+describe('Traveler', () => {
+
+  let testTraveler
+
+  beforeEach(() => {
+
+    testTraveler = new Traveler(travelers[0])
+
+})
+
+it('should be a function', () => {
+  expect(Traveler).to.be.a('function')
+})
+
+it('should be able to instantiate a new traveler', () => {
+  expect(testTraveler).to.be.an.instanceof(Traveler)
+})
+
+});
