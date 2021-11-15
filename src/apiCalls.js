4449f8febcd610e2a37a68857637c385edcd6075
Colgan Meanor <colganmeanor7@gmail.com>
2021-11-14 21:25:37 -0600

    Change traveler

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchTravelers = fetch('http://localhost:3001/api/v1/travelers')
-const fetchSingleTraveler = fetch('http://localhost:3001/api/v1/travelers/3')
+const fetchSingleTraveler = fetch('http://localhost:3001/api/v1/travelers/4')
 const fetchTrips = fetch('http://localhost:3001/api/v1/trips')
 const fetchDestinations = fetch('http://localhost:3001/api/v1/destinations')
 
